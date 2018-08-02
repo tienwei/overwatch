08b8fddad9098fba4fc6341eef624ed23d64e5a4
Robert Chang <cht8687@gmail.com>
2018-08-02 18:42:02 +1000

    remove rubbish

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,8 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import Switch from "@material-ui/core/Switch";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormGroup from "@material-ui/core/FormGroup";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
-import logo from "../logo.png";
 
 const styles = {
   root: {
