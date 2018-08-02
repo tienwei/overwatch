7f0720b7c2f08e725d4ebb1b8b6a33f753dbf881
Robert Chang <cht8687@gmail.com>
2018-08-02 22:49:48 +1000

    location api is ready

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,35 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {withStyles} from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = {
-    root: {
-        display: "flex"
-    },
-    flex: {
-        flexGrow: 1
-    },
-    video: {
-        width: "100%",
-        height: "100%"
-    }
+  root: {
+    display: "flex"
+  },
+  flex: {
+    flexGrow: 1
+  },
+  video: {
+    width: "100%",
+    height: "100%"
+  }
 };
 
 class Stream extends React.Component {
-    render() {
-        const {classes, streamUrl} = this.props;
+  render() {
+    const { classes, streamUrl } = this.props;
 
-        return (
-            <div className={classes.root}>
-                <video className={classes.video} controls autoPlay src={streamUrl}
-                       type="application/x-mpegURL"/>
-            </div>
-        )
-    }
+    return (
+      <div className={classes.root}>
+        <video
+          className={classes.video}
+          controls
+          autoPlay
+          src={streamUrl}
+          type="application/x-mpegURL"
+        />
+      </div>
+    );
+  }
 }
 
 Stream.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Stream);
