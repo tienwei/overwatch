8b160457fff403884918c1568ab9dfd22309d189
Robert Chang <cht8687@gmail.com>
2018-08-03 11:28:33 +1000

    username fix

--- src/gmap.js
+++ src/gmap.js
@@ -1,39 +1,40 @@
-import { compose } from 'recompose';
-import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
-import qs from 'qs';
-import { MarkerWithLabel } from 'react-google-maps/lib/components/addons/MarkerWithLabel';
-import { recordLocation } from './services/locations';
-import { hackHumanSvg, hackSvg } from './assets/hackSvg';
+import { compose } from "recompose";
+import React from "react";
+import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
+import qs from "qs";
+import { MarkerWithLabel } from "react-google-maps/lib/components/addons/MarkerWithLabel";
+import { recordLocation } from "./services/locations";
+import { hackHumanSvg, hackSvg } from "./assets/hackSvg";
 
-export const MapWithAMarkerWithLabel = compose(withScriptjs, withGoogleMap)(
-  props => {
-    const { lat: intLat, lng: intLng, username } = qs.parse(
-      window.location.search.slice(1)
-    );
-    const lat = parseFloat(intLat);
-    const lng = parseFloat(intLng);
+export const MapWithAMarkerWithLabel = compose(
+  withScriptjs,
+  withGoogleMap
+)(props => {
+  const { lat: intLat, lng: intLng, username } = qs.parse(
+    window.location.search.slice(1)
+  );
+  const lat = parseFloat(intLat);
+  const lng = parseFloat(intLng);
 
-    const payLoad = {
-      userId: username,
-      latitude: lat,
-      longitude: lng
-    };
+  const payLoad = {
+    userName: username,
+    latitude: lat,
+    longitude: lng
+  };
 
-    recordLocation(payLoad);
+  recordLocation(payLoad);
 
-    return (
-      <GoogleMap defaultZoom={14} defaultCenter={{ lat, lng }}>
-        <MarkerWithLabel position={{ lat, lng }}>
-          <div>
-            <div className="svgContainer">
-              {hackHumanSvg}
-              {hackSvg}
-            </div>
-            <div className="nameContainer">{username} is here</div>
+  return (
+    <GoogleMap defaultZoom={14} defaultCenter={{ lat, lng }}>
+      <MarkerWithLabel position={{ lat, lng }}>
+        <div>
+          <div className="svgContainer">
+            {hackHumanSvg}
+            {hackSvg}
           </div>
-        </MarkerWithLabel>
-      </GoogleMap>
-    );
-  }
-);
+          <div className="nameContainer">{username} is here</div>
+        </div>
+      </MarkerWithLabel>
+    </GoogleMap>
+  );
+});
